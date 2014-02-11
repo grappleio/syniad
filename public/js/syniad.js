1366647bc3c41b9ca8618c179d4e1682650456ce
Nick Johnson <nick.johnson@shonborn-becker.com>
2014-02-11 15:47:26 -0500

    WIP

---
+++ public/js/syniad.js
@@ -0,0 +1,18 @@
+/**
+ * syniad.js
+ */
+
+var Canvas = new Backbone.View.extend({
+	tagName: 'div',
+
+	id:'canvas',
+	events :{
+		"keypress" : "handleKeys"
+	},
+	render: function(){
+
+	},
+	handleKeys(){
+	
+	}
+});
